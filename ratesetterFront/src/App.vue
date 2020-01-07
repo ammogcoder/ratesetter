22cd5b189a900ed0a8699d406ad93530082474c2
Gbenga M. Akomolafe <gmakomolafe@sidmach.com>
2020-01-07 17:01:45 +0100

    fresh push

---
+++ ratesetterFront/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <SignupForm/>
+  </div>
+</template>
+
+<script>
+import SignupForm from './components/SignupForm.vue'
+
+export default {
+  name: 'app',
+  components: {
+    SignupForm
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  /* text-align: center; */
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
